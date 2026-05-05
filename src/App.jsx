866a2b81236f41b124bbbc5545852911c777b25e
Joel Wise <joelwise.ndungu@student.moringaschool.com>
2026-05-05 13:58:35 +0300

    Create new Header.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 //app.jsx
 import { useState } from 'react'
 import react from "react"
-import './App.css'
+import ProjectForm from './components/ProjectForm'
+import ProjectList from './components/ProjectList'
+import ProjectData from './components/ProjectData'
 
 //A function containing the main component of the app.
 function App() {
+    const [projects, setProjects] = useState([])
+
+    return (
+        <div className="app">
+          <Header />
+            <h1>Project Management App</h1>
+            <ProjectForm />
+            <ProjectList />
+            <ProjectData />
+        </div>
+    )
 
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+//Header.jsx
+//A function containing the header component of the app.
+function Header() {
+    return (
+        <header className="header">
+            <h1>Project Management App</h1>
+        </header>
+    )
+}
+
+export default Header
